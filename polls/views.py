c2415d5781a6aab9074edc62fa5eaa72f353ae75
zzZ5 <baoju_liu@foxmail.com>
2020-11-23 18:23:01 +0800

    页面细节添加

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, redirect
 def index(request):
     if not request.session.get('is_login', None):
         return redirect('/account/login/')
-    content = {'session': request.session, 'page_home': True}
+    content = {'session': request.session,
+               'page_home': True, 'page_allEquipment': True}
     return render(request, 'polls/index.html', content)
 
 
