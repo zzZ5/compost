cb4d77adb5a3fa8089c38fed115d04d55ee53b1a
zzZ5 <baoju_liu@foxmail.com>
2020-11-29 13:20:04 +0800

    添加等待图标

--- polls/static/polls/js/addEquipment.js
+++ polls/static/polls/js/addEquipment.js
@@ -2,6 +2,8 @@ $(".addEquipment").click(function (){
     console.log("click");
     var add_equipment = $(this);
     var data = {name:add_equipment.attr("name")};
+    add_equipment.hide()
+    $('.' + data["name"]).show()
     if (add_equipment.hasClass('iconjian1')){
         data["action"] = 'remove'
     } else {
@@ -10,25 +12,27 @@ $(".addEquipment").click(function (){
     $.ajax({
         url: "/addEquipment/",
         method: 'GET',
-        dataType: 'text',
+        dataType: 'JSON',
         data: data,
         success:function (result, status) {
             console.log(result);
-            if (result == 'ok'){
+            if (result['Code'] == 'ok'){
                 if(add_equipment.hasClass('iconjian1')){
                     add_equipment.removeClass("iconjian1");
                     add_equipment.addClass("iconjiatianjiakuangxuanduoxuan-8");
                     add_equipment.attr("title","添加到我的设备");
-                    alert("移除成功！");
+                    alert(result['Message']);
                 }else {
                     add_equipment.removeClass("iconjiatianjiakuangxuanduoxuan-8");
                     add_equipment.addClass("iconjian1");
                     add_equipment.attr("title","从我的设备移除");
-                    alert("添加成功！");
+                    alert(result['Message']);
                 }
             }else {
-                alert("error: " + result);
+                alert("error: " + result['Code'] + '\n' + result['Message']);
             }
+            $('.' + data["name"]).hide()
+            add_equipment.show()
         },
         error: function (result){
             alert("Data transmission error！");
