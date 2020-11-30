b8484c389fccf5148eab71365ed23fc5535b7747
zzZ5 <baoju_liu@foxmail.com>
2020-11-30 09:27:00 +0800

    ...

--- equipment/urls.py
+++ equipment/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'equipment'
 urlpatterns = [
-    path('<id>', views.index, name='index'),
-    path('<id>/datas', views.list_data, name='list_data'),
-    path('<id>/modifyEquipment', views.modify_equipment, name='modify_equipment'),
-    path('<id>/download', views.download_data, name='download_data')
+    path('<id>/', views.index, name='index'),
+    path('<id>/datas/', views.list_data, name='list_data'),
+    path('<id>/modifyEquipment/', views.modify_equipment, name='modify_equipment'),
+    path('<id>/download/', views.download_data, name='download_data')
 ]
