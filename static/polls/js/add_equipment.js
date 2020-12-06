f1458321be8017299ff93f82b9bd82d5bf832e42
zzZ5 <baoju_liu@foxmail.com>
2020-12-06 21:27:11 +0800

    修改Code

--- static/polls/js/add_equipment.js
+++ static/polls/js/add_equipment.js
@@ -1,10 +1,10 @@
-$(".add_equipment").click(function (){
+$(".add_equipment").click(function () {
     console.log("click");
     var add_equipment = $(this);
-    var data = {name:add_equipment.attr("name")};
+    var data = { name: add_equipment.attr("name") };
     add_equipment.hide()
     $('#' + data["name"]).show()
-    if (add_equipment.hasClass('iconjian1')){
+    if (add_equipment.hasClass('iconjian1')) {
         data["action"] = 'remove'
     } else {
         data["action"] = 'add'
@@ -14,27 +14,27 @@ $(".add_equipment").click(function (){
         method: 'GET',
         dataType: 'JSON',
         data: data,
-        success:function (result, status) {
+        success: function (result, status) {
             console.log(result);
-            if (result['Code'] == 'ok'){
-                if(add_equipment.hasClass('iconjian1')){
+            if (result['Code'] == '100') {
+                if (add_equipment.hasClass('iconjian1')) {
                     add_equipment.removeClass("iconjian1");
                     add_equipment.addClass("iconjiatianjiakuangxuanduoxuan-8");
-                    add_equipment.attr("title","添加到我的设备");
+                    add_equipment.attr("title", "添加到我的设备");
                     alert(result['Message']);
-                }else {
+                } else {
                     add_equipment.removeClass("iconjiatianjiakuangxuanduoxuan-8");
                     add_equipment.addClass("iconjian1");
-                    add_equipment.attr("title","从我的设备移除");
+                    add_equipment.attr("title", "从我的设备移除");
                     alert(result['Message']);
                 }
-            }else {
+            } else {
                 alert("error: " + result['Code'] + '\n' + result['Message']);
             }
             $('#' + data["name"]).hide()
             add_equipment.show()
         },
-        error: function (result){
+        error: function (result) {
             alert("Data transmission error！");
         }
     });
